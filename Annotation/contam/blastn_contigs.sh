87f0d8863013ea71d863f05486fe7010ea561970
Jason Stajich <jasonstajich.phd@gmail.com>
2022-02-13 21:17:33 -0800

    add steps for screening

---
+++ Annotation/contam/blastn_contigs.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/bash -l
+#SBATCH -p intel,batch -N 1 -n 8 --mem 64gb --out blastn.%A.log
+
+module load ncbi-blast/2.9.0+
+CPU=8
+
+blastn -task dc-megablast -db /srv/projects/db/ncbi/preformatted/20220131/ref_prok_rep_genomes -query bacteria_candidate_contigs.fa -out bacteria_candidate_contigs.refprok.blastn -evalue 1e-3 -num_threads $CPU
